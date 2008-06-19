a1112daadb6b3847c6bfd27c2de731279692a027
roussel.n <roussel.n@2c10d059-f54f-0410-aad8-2b13b8dc14b6>
2008-06-19 21:40:32 +0000

    First release of CdcProductsTestFactory

---
+++ GemShellSuitev3/GemShellSuitev3-ejb/src/java/com/gemalto/gemshellsuitev3/ejb/session/stateless/factory/CdcProductsTestFactoryBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gemalto.gemshellsuitev3.ejb.session.stateless.factory;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author nicolas
+ */
+@Stateless
+public class CdcProductsTestFactoryBean implements CdcProductsTestFactoryLocal {
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "EJB Methods > Add Business Method" or "Web Service > Add Operation")
+ 
+}
---
+++ GemShellSuitev3/GemShellSuitev3-ejb/src/java/com/gemalto/gemshellsuitev3/ejb/session/stateless/factory/CdcProductsTestFactoryLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.gemalto.gemshellsuitev3.ejb.session.stateless.factory;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author nicolas
+ */
+@Local
+public interface CdcProductsTestFactoryLocal {
+    
+}
